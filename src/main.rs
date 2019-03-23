cb240c1983b69d16d3e5782c80ea2be75ca584d7
mattperkins <github@mattperkins.com>
2019-03-23 21:16:51 +0000

    scaffold basic get request route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+#![feature(plugin, custom_derive, const_fn, decl_macro, extern_prelude)]
+#![plugin(rocket_codegen)]
+extern crate rocket;
+extern crate rocket_contrib;
+fn rocket() -> rocket::Rocket {
+    rocket::ignite()
+        .mount("/", routes![index])
+}
+#[get("/")]
+fn index<'a>() -> &'a str {
+    "Hello!"
+}
 fn main() {
-    println!("Hello, world!");
+    rocket().launch();
 }
